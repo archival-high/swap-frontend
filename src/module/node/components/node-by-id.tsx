917bd4062862f03143271b9dcfe090b16a5840bf
a/shopkyo <argens@connect.hku.hk>
2021-06-11 09:34:14 +0800

    drag drop

--- src/module/node/components/node-by-id.tsx
+++ src/module/node/components/node-by-id.tsx
@@ -1,18 +1,23 @@
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../../redux";
 import {NodeDisplay} from "./display/node-display";
-import {Button, Container} from "@material-ui/core";
+import {Box, Button, Container} from "@material-ui/core";
 import {useState} from "react";
 import {deserialize} from "class-transformer";
+import {setParent} from "../../../redux/data";
+import {Draggable} from "../../../components/draggable";
+import {DropZone} from "../../../components/drop-zone";
 
 type Props = {
     id: string
 }
-export function NodeById (props: Props){
+
+export function NodeById(props: Props) {
     const {id} = props;
     const [children, setChildren] = useState<string[]>([]);
     const lookup = useSelector((state: RootState) => state.data.nodes);
     const linkTable = useSelector((state: RootState) => state.data.backlink);
+    const dispatch = useDispatch();
     // console.log(linkTable);
     // console.log(id);
     // if (!id){
@@ -27,23 +32,20 @@ export function NodeById (props: Props){
         setChildren(c);
     }
     return (
-        <div style={{
-            display: 'flex',
-            flexDirection: 'column'
-        }}>
-            <div style={{
-                display: 'flex',
-            }}>
+        <Container>
+            <Box flexDirection={'row'} display={'flex'}>
                 <NodeDisplay node={node}/>
                 <Button onClick={getChildren}>
                     Get Children
                 </Button>
-            </div>
+            </Box>
             <Container>
-                {children.map (child => (
+                {children.map(child => (
                     <NodeById id={child} key={child}/>
                 ))}
             </Container>
-        </div>
+        </Container>
     )
 }
+
+//https://codesandbox.io/s/j4yvnr7n83?file=/src/questions.js
